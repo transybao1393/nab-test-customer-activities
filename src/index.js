c95b5e4f606c67099b25ffe05727ac0af00cd5c1
Tran Sy Bao <transybao28@gmail.com>
2018-09-29 15:36:19 +0700

    Update some libs, remote services

--- src/index.js
+++ src/index.js
@@ -2,12 +2,8 @@ import express from 'express';
 import morgan from 'morgan';
 import compression from 'compression';
 import CORS from 'cors';
-import fs from 'fs';
 import config from './config.json';
-import http from 'http';
-import https from 'https';
 const app = express();
-const httpServer = http.createServer(app);
 
 //- using morgan
 app.use(morgan(':status [:method] :url - :response-time ms'))
@@ -17,19 +13,6 @@ app.use(compression())
 
 //- using CORS for all request
 app.use(CORS())
-
-//- create https server
-const options = 
-{
-    key: fs.readFileSync('test/fixtures/keys/agent2-key.pem'),
-    cert: fs.readFileSync('test/fixtures/keys/agent2-cert.pem')
-};
-
-//- routes
-app.get('/:id', (req, res) => {
-    let param = req.params['id'];
-    res.json({status: 200, message: param})
-})
 
 //- open a server
 app.listen(process.env.PORT || config.port, () => {
