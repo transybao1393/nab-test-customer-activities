1776150afbfc42716b86c3cbe757b2e8038bddf2
Tran Sy Bao <transybao28@gmail.com>
2018-10-22 22:40:33 +0700

    Adding twilio config

---
+++ src/services/twilio.js
@@ -0,0 +1,15 @@
+import config from '../config.json';
+// Download the helper library from https://www.twilio.com/docs/node/install
+// Your Account Sid and Auth Token from twilio.com/console
+const accountSid = config.twilio.ACCOUNT_SID;
+const authToken = config.twilio.AUTH_TOKEN;
+const client = require('twilio')(accountSid, authToken);
+
+client.messages
+.create({
+    body: 'This is the ship that made the Kessel Run in fourteen parsecs?',
+    from: '+15017122661',
+    to: '+15558675310'
+})
+.then(message => console.log(message.sid))
+.done();
