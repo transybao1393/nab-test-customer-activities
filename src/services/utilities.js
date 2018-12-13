da30f98375f6885979e76a0edc728ac162714e15
Tran Sy Bao <transybao28@gmail.com>
2018-12-13 19:44:12 +0700

    Adding randomPass function to support generate random password randomly

--- src/services/utilities.js
+++ src/services/utilities.js
@@ -1,11 +1,19 @@
 import _ from 'lodash';
-
+import Chance from 'chance';
+import moment from 'moment';
 //- example
 const groupBy = async (collection, t) => {
     return _.chain(collection).groupBy(t); 
 }
 
+const randomPass = async () => {
+    let chance = new Chance();
+    return chance.string({
+        length: 13
+    });
+}
 
 export {
-    groupBy
+    groupBy,
+    randomPass
 }
