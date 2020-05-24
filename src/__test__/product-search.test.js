ef7cfbbf472b082d2314f5733a9516bda7b41bd2
bao.tran <bao.tran@jtexpress.sg>
2020-05-24 14:11:36 +0700

    Update test, update github ci

--- src/__test__/product-search.test.js
+++ src/__test__/product-search.test.js
@@ -4,24 +4,18 @@ import "babel-polyfill";
 // import app from '../index';
 // import supertest from 'supertest';
 
-// const app = require('../index');
 import app from '../index';
-const request = require('supertest');
+import supertest from 'supertest';
+let request = supertest(app);
 
 
 import mongoose from 'mongoose';
 
 describe('Product Search Test', () => {
 
-    describe('Sample Test', () => {
-        it('should test that true === true', () => {
-          expect(true).toBe(true);
-        });
-    });
-
     it('Wrong URI', async () => {
         //- incase of wrong uri
-        const response = await request(app).get('/product/any1');
+        const response = await request.get('/product/any1');
         expect(response.status).toBe(404);
         mongoose.connection.close();
     });
@@ -31,12 +25,8 @@ beforeAll(done => {
     return done();
 });
 
-afterEach(function(done) {
-    mongoose.disconnect();
-    return done();
-});
-
 afterAll(done => {
     // Closing the DB connection allows Jest to exit successfully.
+    mongoose.connection.close();
     return done();
 });
