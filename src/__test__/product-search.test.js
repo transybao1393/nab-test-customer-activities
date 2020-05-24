c05ea22c39d50a3d6f6a5c432e342855a2e83160
bao.tran <bao.tran@jtexpress.sg>
2020-05-24 12:47:00 +0700

    Update...

--- src/__test__/product-search.test.js
+++ src/__test__/product-search.test.js
@@ -7,11 +7,12 @@ import mongoose from 'mongoose';
 
 describe('Product Search Test', () => {
 
-    it('Wrong URI', async () => {
+    it('Wrong URI', async (done) => {
         //- incase of wrong uri
         const response = await request.get('/product/any1');
         expect(response.status).toBe(404);
-        // done();
+        mongoose.connection.close();
+        done();
     });
 });
 
