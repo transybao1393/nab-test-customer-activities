b35f7f619ea1532957eb738a487f9b11f8cd7258
bao.tran <bao.tran@jtexpress.sg>
2020-05-26 12:18:32 +0700

    Grant permission for wait-for-it.sh, clone project, create docker network, docker-compose up automatically

--- start.sh
+++ start.sh
@@ -2,14 +2,31 @@
 # Checking docker & folder nab-test
 if [ -x "$(command -v docker)" ] && [ ! -d "/nab-test" ]
 then
+    echo "[x] Cloning process..."
     git clone https://github.com/transybao1393/nab-test.git
+    echo "[x] Installing packages..."
     cd nab-test
     npm install
+    echo "[x] Granted permission for wait-for-it.sh file..."
+    chmod +x wait-for-it.sh
     # Creating network
+    echo "[x] Create docker network and start docker-compose..."
     docker network create bao_network
     docker-compose up
-    echo "Running process. Please wait..."
 else
     echo "Please first install docker using Homebrew (if you are using Mac)"
     echo "Following steps will be list at: https://codingbee.net/docker/install-docker-for-mac-using-homebrew"
+fi
+
+# Check if nab-test folder is exist
+if [ -d "/nab-test" ]
+then
+    cd nab-test
+    npm install
+    echo "[x] Granted permission for wait-for-it.sh file..."
+    chmod +x wait-for-it.sh
+    # Creating network
+    echo "[x] Create docker network and start docker-compose..."
+    docker network create bao_network
+    docker-compose up
 fi
